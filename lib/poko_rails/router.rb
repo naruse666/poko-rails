54a1a370275f1589f6da2a357ac1c49fae640154
naruse666 <0x706f6b6f@gmail.com>
2026-02-01 20:17:09 +0900

    phase1: response route

--- lib/poko_rails/router.rb
+++ lib/poko_rails/router.rb
@@ -10,19 +10,24 @@ module PokoRails
       method = env['REQUEST_METHOD']
       path = env['PATH_INFO']
 
-      matched = @route_set.routes.any? do |r|
+      route = @route_set.routes.find do |r|
         r.http_method == method && r.path == path
       end
 
-      if matched
-        [
-          200, { 'content-type' => 'text/plain; charset=utf-8' }, ["poko-rails: hello\n"]
-        ]
-      else
-        [
-          404, { 'content-type' => 'text/plain; charset=utf-8' }, ["Not Found\n"]
-        ]
-      end
+      return not_found if route.nil?
+
+      puts route
+      [
+        200, { 'content-type' => 'text/plain; charset=utf-8' }, ["to=#{route.to}\n"]
+      ]
+    end
+
+    private
+
+    def not_found
+      [
+        404, { 'content-type' => 'text/plain; charset=utf-8' }, ["Not Found\n"]
+      ]
     end
   end
 end
